d46f855c01dbedbf6e15d606d15bce2b3a1ef2af
Khang Nguyen <ngkhang0220@gmail.com>
2026-01-16 15:02:16 +0700

    chore(format): integrate Prettier
    - Add Prettier with config and scripts
    - Adjust line length and update formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  endOfLine: "lf",
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  proseWrap: "always",
+  bracketSpacing: true,
+};
